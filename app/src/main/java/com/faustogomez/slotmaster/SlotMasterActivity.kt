091c956aa6306e40c81c654a17795c206849cf0f
Fausto Gomez <gomez_fausto@hotmail.com>
2020-02-25 15:56:26 -0800

    add slotmaster view

--- app/src/main/java/com/faustogomez/slotmaster/SlotMasterActivity.kt
+++ app/src/main/java/com/faustogomez/slotmaster/SlotMasterActivity.kt
@@ -2,12 +2,34 @@ package com.faustogomez.slotmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_slotmaster.*
 
 class SlotMasterActivity : AppCompatActivity() {
+
+    private val slot: List<Slot> = listOf(
+        Slot(R.drawable.cherries, R.drawable.dollarsign, R.drawable.grapes),
+        Slot(R.drawable.dollarsign, R.drawable.cherries, R.drawable.cherries),
+        Slot(R.drawable.grapes, R.drawable.grapes, R.drawable.dollarsign),
+        Slot(R.drawable.lemon, R.drawable.lemon, R.drawable.lemon),
+        Slot(R.drawable.number, R.drawable.number, R.drawable.number)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slotmaster)
+
+        spinSlots()
+
+        spin_button.setOnClickListener(){
+            spinSlots()
+        }
+    }
+
+    private fun spinSlots() {
+        leftImage.setImageResource(slot.random().leftImage)
+        centerImage.setImageResource(slot.random().centerImage)
+        rightImage.setImageResource(slot.random().rightImage)
     }
 }
 
+data class Slot(val leftImage: Int, val centerImage: Int, val rightImage: Int)
